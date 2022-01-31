4267feb22c0f72fa423c91caac4929f2cce1cc9e
Yuliya <vsggdfaf@gmail.com>
2022-01-31 17:31:50 +0300

    изменение в html и js

---
+++ modules/one.js
@@ -0,0 +1,10 @@
+const one = () => {
+    const timerHours = document.getElementById('timer-hours')
+    const timerMinutes = document.getElementById('timer-minutes')
+    const timerSeconds = document.getElementById('timer-seconds')
+    //timer-hours
+    //timer-minutes
+    //timer-seconds
+}
+
+export default one
