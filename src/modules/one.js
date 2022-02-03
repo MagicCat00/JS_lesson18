65563321ad92c61604a2101cf46cb1bf648b5f51
Yuliya <vsggdfaf@gmail.com>
2022-02-03 15:46:43 +0300

    вывод в консоль часов, минут и секунд

--- src/modules/one.js
+++ src/modules/one.js
@@ -2,12 +2,19 @@ const one = () => {
     const timerHours = document.getElementById('timer-hours');
     const timerMinutes = document.getElementById('timer-minutes');
     const timerSeconds = document.getElementById('timer-seconds');
-    console.log(timerHours);
-    console.log(timerMinutes);
-    console.log(timerSeconds);
-    //timer-hours
-    //timer-minutes
-    //timer-seconds
+ 
+    let deadline = '10 february 2022'
+    let dateStop = new Date(deadline).getTime()
+    let dateNow = new Date().getTime()
+    let timeRemaining = (dateStop - dateNow) / 1000
+    let hours = Math.floor(timeRemaining / 60 / 60)
+    let minutes = Math.floor(timeRemaining / 60)
+    let seconds = Math.floor(timeRemaining)
+    
+    console.log(hours);
+    console.log(minutes);
+    console.log(seconds);
+    
 }
 
 export default one
